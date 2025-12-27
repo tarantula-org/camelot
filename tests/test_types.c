6e924fb7833be2c19e92797834b990ac677368de
AKRiLLiCK <akrillick@proton.me>
2025-12-27 10:46:37 +0100

    Added MPL and ARS headers

--- tests/test_types.c
+++ tests/test_types.c
@@ -1,3 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Governed by The Architectural Rigor Standard (ARS-1.0).
+ * Compliance is mandatory for all contributions.
+*/
+
 #include "tests.h"
 #include "camelot.h"
 #include <string.h>
